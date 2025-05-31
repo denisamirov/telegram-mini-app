f7943293ff1f56b8abe4e100ed000df465b312d6
denis <amirowdenis@yandex.ru>
2025-05-31 13:53:04 +0300

    feat: banner-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
